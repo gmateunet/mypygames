9aaeb58e87ba674587e500c43137ff7965cb1a64
Tu Nombre <you@example.com>
2024-05-18 12:02:26 +0200

    caminos pre generados

--- maze_definition.py
+++ maze_definition.py
@@ -1,45 +1,17 @@
 from globals import *
+from mazes import mazes
 import random
 
 class Maze:
     def __init__(self, rows=15, cols=20):
         self.rows = rows
         self.cols = cols
-        self.maze = self.generate_maze(rows, cols)
+        self.maze = self.select_random_maze()
         self.total_pills = sum(row.count('.') for row in self.maze)
         self.valid_positions = self.get_valid_positions()
 
-    def generate_maze(self, rows, cols):
-        # Inicializar el laberinto con paredes
-        maze = [['#' for _ in range(cols)] for _ in range(rows)]
-        
-        # Elegir un punto inicial
-        start_row, start_col = 1, 1
-        maze[start_row][start_col] = '.'
-
-        # Lista de direcciones (arriba, abajo, izquierda, derecha)
-        directions = [(-2, 0), (2, 0), (0, -2), (0, 2)]
-
-        def is_valid(row, col):
-            return 0 <= row < rows and 0 <= col < cols
-
-        def carve_path(row, col):
-            random.shuffle(directions)
-            for dr, dc in directions:
-                new_row, new_col = row + dr, col + dc
-                if is_valid(new_row, new_col) and maze[new_row][new_col] == '#':
-                    wall_row, wall_col = row + dr // 2, col + dc // 2
-                    maze[wall_row][wall_col] = '.'
-                    maze[new_row][new_col] = '.'
-                    carve_path(new_row, new_col)
-
-        # Empezar a tallar el laberinto desde el punto inicial
-        carve_path(start_row, start_col)
-
-        # Convertir el laberinto a una lista de cadenas
-        maze_str = [''.join(row) for row in maze]
-
-        return maze_str
+    def select_random_maze(self):
+        return random.choice(mazes)
 
     def get_valid_positions(self):
         valid_positions = []
